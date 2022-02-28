75228d60d36880eb091b4413a20c3dc6ce240d90
mahabub ahmed <mahabub.ahmed.eee@gmail.com>
2022-02-28 22:48:49 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const searchMobile = () => {
+  const searhField = document.getElementById("search-field");
+  const searchText = searhField.value;
+  //   console.log(searchText);
+
+  searhField.value = "";
+};
