529633f9f6d99d0b39a97d6a49d08be4c0db72b9
mahabub ahmed <mahabub.ahmed.eee@gmail.com>
2022-03-01 22:18:38 +0100

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,71 @@
 const searchMobile = () => {
   const searhField = document.getElementById("search-field");
   const searchText = searhField.value;
-  //   console.log(searchText);
-
+  //   clear data
   searhField.value = "";
+  if (searchText == "") {
+    // Please write some thing to display
+  } else {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    //   console.log(url);
+    fetch(url)
+      .then(res => res.json())
+      .then(info => displaySearchResult(info.data));
+  }
+};
+
+const displaySearchResult = data => {
+  const searchResult = document.getElementById("search-result");
+  searchResult.textContent = "";
+  if (data.length == 0) {
+    // no result found
+  }
+  data.forEach(data => {
+    // console.log(data);
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
+        <div class="card h-100 container py-3">
+          <img src="${data.image}" class="card-img-top h-60" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title fw-bold">${data.brand}</h5>
+            <p class="card-text fw-bolder">${data.phone_name}
+            </p>
+          </div>
+          <button onclick="loadMobileDetails('${data.slug}')" type="button" class="btn btn-primary mx-auto" style =" width: 85px">Details</button>
+        </div>
+        
+    `;
+    searchResult.appendChild(div);
+  });
+};
+
+const loadMobileDetails = mobileId => {
+  const url = `https://openapi.programming-hero.com/api/phone/${mobileId}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayMobileDetail(data.data));
+};
+
+const displayMobileDetail = mobile => {
+  console.log(mobile);
+
+  const mobileDetails = document.getElementById("mobile-details");
+  const div = document.createElement("div");
+  div.classList.add("card");
+  div.innerHTML = `
+        <img src="${mobile.image}" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">${mobile.name}</h5>
+          </p>
+          <p class="card-text">
+          </p>
+          <p class="card-text">
+          </p>
+          <p class="card-text">
+          </p>
+          
+        </div>
+  `;
+  mobileDetails.appendChild(div);
 };
